78580e0464bb0f3ee8052b363b42fdf2dec6fff7
IvanMathyn <winter.nes@gmail.com>
2021-12-12 17:10:28 +0200

    Traffic lights app hw

--- TraficLights/ViewController.swift
+++ TraficLights/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
+enum TrafficLights {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redTrafficView: UIView!
+    @IBOutlet weak var yellowTrafficView: UIView!
+    @IBOutlet weak var greenTrafficView: UIView!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    private var activeTrafficColor = TrafficLights.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.layer.cornerRadius = 15
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redTrafficView.layer.cornerRadius = redTrafficView.frame.width / 2
+        yellowTrafficView.layer.cornerRadius = redTrafficView.frame.width / 2
+        greenTrafficView.layer.cornerRadius = redTrafficView.frame.width / 2
     }
 
-
+    @IBAction func buttonHandler(_ sender: Any) {
+        button.setTitle("Next", for: .normal)
+    
+        setTrafficActive()
+    }
+    
+    private func setTrafficActive() -> Void {
+        switch activeTrafficColor {
+        case .red:
+            redTrafficView.alpha = lightIsOn
+            yellowTrafficView.alpha = lightIsOff
+            greenTrafficView.alpha = lightIsOff
+            activeTrafficColor = TrafficLights.yellow
+        case .yellow:
+            activeTrafficColor = TrafficLights.green
+            redTrafficView.alpha = lightIsOff
+            yellowTrafficView.alpha = lightIsOn
+            greenTrafficView.alpha = lightIsOff
+        case .green:
+            activeTrafficColor = TrafficLights.red
+            redTrafficView.alpha = lightIsOff
+            yellowTrafficView.alpha = lightIsOff
+            greenTrafficView.alpha = lightIsOn
+        }
+        
+    }
+    
 }
 
